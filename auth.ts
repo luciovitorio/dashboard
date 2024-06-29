acf10c26a0a17e203b778ffb2e83ae55b38c2fdf
Lucio Vitorio <luciovitorio@yahoo.com.br>
2024-06-28 22:23:02 -0300

    terceiro commit

--- auth.ts
+++ auth.ts
@@ -1,9 +1,7 @@
 import NextAuth from "next-auth";
-import authConfig from "./auth.config";
-// import Credentials from "next-auth/providers/credentials";
+import authConfig from "@/auth.config";
 import { PrismaClient } from "@prisma/client";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-// import { compareSync } from "bcrypt-ts";
 import db from "./lib/db";
 
 // const prisma = new PrismaClient();
@@ -12,35 +10,35 @@ export const {
   handlers: { GET, POST },
   auth,
   signIn,
-  // signOut,
+  signOut,
 } = NextAuth({
   ...authConfig,
   adapter: PrismaAdapter(db),
   session: {
     strategy: "jwt",
   },
-  providers: [
-    // Credentials({
-    //   credentials: {
-    //     email: {},
-    //     password: {},
-    //   },
-    //   authorize: async (credentials) => {
-    //     const email = credentials.email as string;
-    //     const password = credentials.password as string;
-    //     if (!email || !password) {
-    //       return null;
-    //     }
-    //     const user = await db.user.findUnique({
-    //       where: {
-    //         email,
-    //       },
-    //     });
-    //     if (!user) return null;
-    //     const matches = compareSync(password, user.password ?? "");
-    //     if (matches) return user;
-    //     return null;
-    //   },
-    // }),
-  ],
+  // providers: [
+  // Credentials({
+  //   credentials: {
+  //     email: {},
+  //     password: {},
+  //   },
+  //   authorize: async (credentials) => {
+  //     const email = credentials.email as string;
+  //     const password = credentials.password as string;
+  //     if (!email || !password) {
+  //       return null;
+  //     }
+  //     const user = await db.user.findUnique({
+  //       where: {
+  //         email,
+  //       },
+  //     });
+  //     if (!user) return null;
+  //     const matches = compareSync(password, user.password ?? "");
+  //     if (matches) return user;
+  //     return null;
+  //   },
+  // }),
+  // ],
 });
--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,39 @@
 import NextAuth from "next-auth";
-import authConfig from "./auth.config";
+import authConfig from "@/auth.config";
+import {
+  DEFAULT_LOGIN_REDIRECT,
+  apiAuthPrefix,
+  authRoutes,
+  publicRoutes,
+} from "@/routes";
 
 const { auth } = NextAuth(authConfig);
 
 // Função que é executada toda vez que a rota da MATCH no objeto config abaixo
 export default auth((req) => {
+  const { nextUrl } = req;
   const isLoggedIn = !!req.auth;
-  console.log("Route:", req.nextUrl.pathname);
-  console.log("Is logged:", isLoggedIn);
+
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+  if (isApiAuthRoute) {
+    return;
+  }
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    }
+    return;
+  }
+
+  if (!isLoggedIn && !isPublicRoute) {
+    return Response.redirect(new URL("/login", nextUrl));
+  }
+
+  return;
 });
 
 // Da Match na rota que eu quero que faça algo
